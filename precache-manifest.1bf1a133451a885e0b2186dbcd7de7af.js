f9092c3574e0044c85be2d2b184a4fb939d7da62
Andreas_Just <andreas.wq@gmail.com>
2020-04-26 05:21:29 +0300

    Updates

---
+++ precache-manifest.1bf1a133451a885e0b2186dbcd7de7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f4a9a667ffb819367cb4d4aee96185",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ca56649be5f34e8805e",
+    "url": "./static/css/main.edaf781c.chunk.css"
+  },
+  {
+    "revision": "c6ffb54d3bd7723c057f",
+    "url": "./static/js/2.92b4fa3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.92b4fa3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca56649be5f34e8805e",
+    "url": "./static/js/main.655f8337.chunk.js"
+  },
+  {
+    "revision": "e441eccd62b96612a0ea",
+    "url": "./static/js/runtime-main.55b2265f.js"
+  }
+]);
